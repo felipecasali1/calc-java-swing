fba24b0afa8454d9e7e2e6f846e3e898531374a3
felipecasali1 <felipecasali2604@gmail.com>
2025-03-02 22:18:00 -0400

    Subindo arquivos

---
+++ src/view/TelaCalc.java
@@ -0,0 +1,48 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class TelaCalc extends JFrame {
+    private JPanel jpDisplay;
+    private JTextField jtfDisplay;
+    
+    private JPanel jpNumbers;
+    private JButton jbButton0;
+    private JButton jbButton1;
+    private JButton jbButton2;
+    private JButton jbButton3;
+    private JButton jbButton4;
+    private JButton jbButton5;
+    private JButton jbButton6;
+    private JButton jbButton7;
+    private JButton jbButton8;
+    private JButton jbButton9;
+    private JButton jbComma;
+    private JButton jbEquals;
+    
+    private JPanel jpOperations;
+    private JButton jbClear;
+    private JButton jbAdd;
+    private JButton jbSub;
+    private JButton jbMult;
+    private JButton jbDiv;
+    
+    public TelaCalc() {
+        init();
+        actions();
+    }
+    
+    private void init() {
+        setVisible(true);
+        setSize(360, 480);
+        setTitle("Calculadora");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    private void actions() {
+        
+    }
+}
