6fd571d8c14981af2bdcc7de5baf02dadb351d8d
felipecasali1 <felipecasali2604@gmail.com>
2025-03-04 18:04:16 -0400

    Adicionando botoes

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,6 @@ import view.TelaCalc;
 
 public class Main {
     public static void main(String[] args) {
-        new TelaCalc();
+        new TelaCalc().setVisible(true);
     }
 }
--- src/view/TelaCalc.java
+++ src/view/TelaCalc.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -39,14 +42,67 @@ public class TelaCalc extends JFrame {
     }
     
     private void init() {
-        setVisible(true);
         setSize(360, 480);
         setTitle("Calculadora");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
         
+        jpDisplay = new JPanel();
+        jpButtons = new JPanel(new BorderLayout(8, 8));
+        jpNumbers = new JPanel(new GridLayout(4, 3, 8, 8));
+        jpOperations = new JPanel(new GridLayout(5, 1, 8, 8));
         
+        jtfDisplay = new JTextField();
+        
+        jbButton0 = new JButton("0");
+        jbButton1 = new JButton("1");
+        jbButton2 = new JButton("2");
+        jbButton3 = new JButton("3");
+        jbButton4 = new JButton("4");
+        jbButton5 = new JButton("5");
+        jbButton6 = new JButton("6");
+        jbButton7 = new JButton("7");
+        jbButton8 = new JButton("8");
+        jbButton9 = new JButton("9");
+        jbComma = new JButton(",");
+        jbEquals = new JButton("=");
+        
+        jbClear = new JButton("C");
+        jbAdd = new JButton("+");
+        jbSub = new JButton("-");
+        jbMult = new JButton("*");
+        jbDiv = new JButton("/");
+        
+        jtfDisplay.setHorizontalAlignment(JTextField.RIGHT);
+        jtfDisplay.setPreferredSize(new Dimension(320, 60));
+        
+        jpDisplay.add(jtfDisplay);
+        jpButtons.add(jpNumbers, BorderLayout.WEST);
+        jpButtons.add(jpOperations, BorderLayout.EAST);
+        jpDisplay.add(jtfDisplay);
+        
+        jpNumbers.add(jbButton7);
+        jpNumbers.add(jbButton8);
+        jpNumbers.add(jbButton9);
+        jpNumbers.add(jbButton4);
+        jpNumbers.add(jbButton5);
+        jpNumbers.add(jbButton6);
+        jpNumbers.add(jbButton1);
+        jpNumbers.add(jbButton2);
+        jpNumbers.add(jbButton3);
+        jpNumbers.add(jbButton0);
+        jpNumbers.add(jbComma);
+        jpNumbers.add(jbEquals);
+        
+        jpOperations.add(jbClear);
+        jpOperations.add(jbAdd);
+        jpOperations.add(jbSub);
+        jpOperations.add(jbMult);
+        jpOperations.add(jbDiv);
+        
+        add(jpDisplay, BorderLayout.NORTH);
+        add(jpButtons, BorderLayout.CENTER);
     }
     
     private void actions() {
